52a655b4930eda89587909fc95d48fcdeadcccba
lholsen96 <42640397+lholsen96@users.noreply.github.com>
2018-09-06 15:58:37 +0200

    Update name_banker.py
    La til CV

--- src/project-1/name_banker.py
+++ src/project-1/name_banker.py
@@ -15,13 +15,34 @@ class NameBanker:
     def fit(self, X, y):
        neighbor_ks = range(1,100)
        untrained_models = [KNeighborsClassifier(n_neighbors=k) for k in neighbor]
-       k_fold_scores = [cross_val_score(estimator=m), X = df_scaled[features], y = df_scaled[target], cv = 5]
+        
+        features = ['checking account balance', 'duration', 'credit history',
+            'purpose', 'amount', 'savings', 'employment', 'installment',
+            'marital status', 'other debtors', 'residence time',
+            'property', 'age', 'other installments', 'housing', 'credits',
+            'job', 'persons', 'phone', 'foreign']
+        target = 'repaid'
+        
+        k_fold_scores = [cross_val_score(estimator=m), X = features, y = target, cv = 5] #skal vi ha med scaled?
+        
+        mean_cv_scores = [score.mean() for score in k_fold_scores] 
+        plt.errorbar(neighbor_ks, mean_cv_scores, yerr=[score.std() for score in k_fold_scores])
+        
+        knn_best_k_cv = numpy.asarray(mean_cv_scores).argmax()
+        knn_best_k_train = numpy.asarray(train_scores).argmax()
+        knn_best_k_test = numpy.asarray(test_scores).argmax()
+        print(ks[knn_best_k_cv], ks[knn_best_k_train], ks[knn_best_k_test])
+        plt.semilogx(ks, train_scores, ks, test_scores, ks, mean_xv_scores)
+        plt.legend(["Train", "Test", "XV"])
+
+        # Let's select the best model on the basis of the XV score, as we must, since the 'test' result is invisible to us
+        knn_best_model_cv = models[knn_best_k_cv]
+        
 
 
 
 
-
-        neigh = KNeighborsClassifier(n_neighbors = 4)
+        neigh = KNeighborsClassifier(n_neighbors = knn_best_k_cv)
         self.model = neigh.fit(X,y)
         return self.model
         
