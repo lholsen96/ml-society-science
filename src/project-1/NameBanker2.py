4406981c480686d032705e41f445d9a2aa3796c0
lholsen96 <42640397+lholsen96@users.noreply.github.com>
2018-09-10 13:08:17 +0200

    Add files via upload

---
+++ src/project-1/NameBanker2.py
@@ -0,0 +1,70 @@
+
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+
+class NameBanker2: 
+
+    def __init__(self, model):
+        self.model = model
+
+
+    
+    # Fit the model to the data.  You can use any model you like to do
+    # the fit, however you should be able to predict all class
+    # probabilities
+    def fit(self, X, y):
+        self.model = self.model.fit(X,y)
+        
+        
+        
+    
+    # set the interest rate
+    def set_interest_rate(self, rate):
+        self.rate = rate
+
+
+    # Predict the probability of success for a specific person with data x
+    def predict_proba(self, x):
+        # We use the predict_proba function in the sklearn.neighbors.KNeighborsClassifier class.
+        # Note that we add '[0,0]' at the end to only get the prob of success
+        
+        print("Probablilities for suc and fail: ", self.model.predict_proba(x)[0])
+        return  self.model.predict_proba(x)[0,0]
+        
+
+    # THe expected utility of granting the loan or not. Here there are two actions:
+    # action = 0 do not grant the loan
+    # action = 1 grant the loan
+    #
+    # Make sure that you extract the length_of_loan from the
+    # 2nd attribute of x. Then the return if the loan is paid off to you is amount_of_loan*(1 + rate)^length_of_loan
+    # The return if the loan is not paid off is -amount_of_loan.
+    def expected_utility(self, x, action):
+        
+        # Har han skrevet feil formel over? Han har glemt -1
+        
+        # We do not grant the loan. Thus, the utility it set equal to zero.
+        if action == 0:
+            return 0
+        
+        # We grant the loan
+        else:
+            success = self.predict_proba([x])
+            failure = 1-success
+            amount = x['amount']
+            duration = x['duration']
+            returnValue = -failure*amount + success*amount*(pow(1 + self.rate, duration) - 1) 
+            #print ("Expected util: %.2f \t ", % returnValue, "Success: ", success, "Amount: ", amount, "Duration: ", duration, "\n")
+            print ("Util: %.2f \t Success: %.2f Amount: %d Duration: %d \n" % (returnValue, success, amount, duration))
+            return returnValue
+
+    # Here we calculate the best action based on which action returns the highest
+    # expected utility
+    def get_best_action(self, x):
+        if self.expected_utility(x, 0) > self.expected_utility(x, 1): 
+            return 0
+        else: 
+            return 1     
+    
--- src/project-1/TestLending.py
+++ src/project-1/TestLending.py
@@ -7,8 +7,10 @@ features = ['checking account balance', 'duration', 'credit history',
             'property', 'age', 'other installments', 'housing', 'credits',
             'job', 'persons', 'phone', 'foreign']
 target = 'repaid'
-df = pandas.read_csv('../../data/credit/german.data', sep=' ',
+df = pandas.read_csv('german.data', sep=' ',
                      names=features+[target])
+
+
 
 
 import matplotlib.pyplot as plt
@@ -30,6 +32,7 @@ def test_decision_maker(X_test, y_test, interest_rate, decision_maker):
         amount = X_test['amount'].iloc[t]
         # If we don't grant the loan then nothing happens
         if (action==1):
+            print("Amount: %.2f PaidBack: %d" %(amount, good_loan))
             if (good_loan == 1):
                 utility -= amount
             else:    
@@ -47,24 +50,71 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import linear_model
 from sklearn.neural_network import MLPClassifier
+
+
 mlp = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(16, 4, 2), random_state=1)
 bagging = BaggingClassifier(KNeighborsClassifier(), n_estimators=10)
 knn = KNeighborsClassifier()
 logistic = linear_model.LogisticRegression()
-#decision_maker = reference_banker.ReferenceBanker(bagging)
+
+"""
+import NameBanker2
+decision_maker = NameBanker2.NameBanker2(bagging)
+interest_rate = 0.005
+
+from sklearn.model_selection import train_test_split
+n_tests2 = 10
+utility2 = 0.0
+for iter in range(n_tests2):
+    X_train, X_test, y_train, y_test = train_test_split(X[encoded_features], X[target], test_size=0.2)
+    decision_maker.set_interest_rate(interest_rate)
+    print("fit", iter)
+    decision_maker.fit(X_train, y_train)
+    utility2 += test_decision_maker(X_test, y_test, interest_rate, decision_maker)
+
+print(utility2 / n_tests2)
+
+"""
+    
+
+
+
 import random_banker
 decision_maker = random_banker.RandomBanker()
+interest_rate = 0.005
+
+from sklearn.model_selection import train_test_split
+n_tests1 = 10
+utility1 = 0
+for iter in range(n_tests1):
+    X_train, X_test, y_train, y_test = train_test_split(X[encoded_features], X[target], test_size=0.2)
+    decision_maker.set_interest_rate(interest_rate)
+    print("fit", iter)
+    decision_maker.fit(X_train, y_train)
+    utility1 += test_decision_maker(X_test, y_test, interest_rate, decision_maker)
+
+print(utility1 / n_tests1)
+
+
+print("NameBanker")
+import NameBanker
+decision_maker = NameBanker.NameBanker()
 interest_rate = 0.05
 
 from sklearn.model_selection import train_test_split
-n_tests = 10
-utility = 0
+n_tests = 1
+utility = 0.0
 for iter in range(n_tests):
-    X_train, X_test, y_train, y_test = train_test_split(X[encoded_features], X[target], test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X[encoded_features], X[target], test_size=0.01)
     decision_maker.set_interest_rate(interest_rate)
+    print("fit", iter)
     decision_maker.fit(X_train, y_train)
+    
     utility += test_decision_maker(X_test, y_test, interest_rate, decision_maker)
 
 print(utility / n_tests)
+print(utility1 / n_tests1)
+
+#print(decision_maker.predict_proba([["A11", "6", "A34", "A43", "1169", "A65", "A75", "4", "A93", "A101", "4", "A121", "67", "A143", "A152", "2", "A173", "1", "A192", "A201"]]))
 
 
